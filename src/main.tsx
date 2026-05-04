30dbff17f4853056bd673eb06f5b72deeefb759d
Jami Harju <jami.harju@icloud.com>
2026-05-04 10:58:42 +0300

    Remove unused responsive stylesheet import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import AppFinal from './AppFinal.tsx';
 import './index.css';
-import './premium-responsive.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
