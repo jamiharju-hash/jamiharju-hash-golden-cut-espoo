70699a208a3069fc9a99d6717e1dab38fcaa8c90
Jami Harju <jami.harju@icloud.com>
2026-05-04 08:06:18 +0300

    Use full stack app entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import AppFullStack from './AppFullStack.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppFullStack />
   </StrictMode>,
 );
