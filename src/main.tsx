6302016c8a171ebe8d60435ca7ca482d25541556
Jami Harju <jami.harju@icloud.com>
2026-05-04 09:35:11 +0300

    Use final owner aligned mobile first app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import AppPremium from './AppPremium.tsx';
+import AppFinal from './AppFinal.tsx';
 import './index.css';
 import './premium-responsive.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppPremium />
+    <AppFinal />
   </StrictMode>,
 );
