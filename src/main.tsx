d530194e9282f423a83ced9ad5ddb98ccae39b42
Jami Harju <jami.harju@icloud.com>
2026-05-04 08:27:14 +0300

    Use authentic social proof app entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import AppFullStack from './AppFullStack.tsx';
+import AppAuthentic from './AppAuthentic.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppFullStack />
+    <AppAuthentic />
   </StrictMode>,
 );
