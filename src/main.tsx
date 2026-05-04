825ac98b089c5dc315063e207c9914962eaec4ca
Jami Harju <jami.harju@icloud.com>
2026-05-04 12:44:35 +0300

    Use launch checked Golden Cut landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import AppWalkIn from './AppWalkIn.tsx';
+import AppLaunch from './AppLaunch.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppWalkIn />
+    <AppLaunch />
   </StrictMode>,
 );
