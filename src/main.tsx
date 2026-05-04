d5b4c314b0b4aee2b045ebd30855aab2958278f2
Jami Harju <jami.harju@icloud.com>
2026-05-04 11:31:30 +0300

    Use walk-in only editorial landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import AppFinal from './AppFinal.tsx';
+import AppWalkIn from './AppWalkIn.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppFinal />
+    <AppWalkIn />
   </StrictMode>,
 );
