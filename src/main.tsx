1724126f8fdabc55ac6edcecead544cd1808b2f8
Jami Harju <jami.harju@icloud.com>
2026-05-04 08:31:44 +0300

    Use polished production app entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import AppAuthentic from './AppAuthentic.tsx';
+import AppProduction from './AppProduction.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppAuthentic />
+    <AppProduction />
   </StrictMode>,
 );
