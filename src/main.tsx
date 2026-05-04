a181663f387b601e28ffceaf573d46e2c5268e62
Jami Harju <jami.harju@icloud.com>
2026-05-04 09:30:14 +0300

    Load final responsive premium polish stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import AppPremium from './AppPremium.tsx';
 import './index.css';
+import './premium-responsive.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
